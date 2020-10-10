767dcdeeac4c0d10ee8bbbbf0473e90eb8dd5545
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-10 12:46:13 +0530

    [Gaurav]ADDED:Created Simple Linked List

--- src/main/java/com/bridgelabz/datastructure/MyNode.java
+++ src/main/java/com/bridgelabz/datastructure/MyNode.java
@@ -1,8 +1,24 @@
 package com.bridgelabz.datastructure;
 
-public class MyNode {
+public class MyNode<K> {
+    private  MyNode next;
+    private  K key;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
     public boolean printWelcomeMessage() {
         System.out.println("Welcome To Linear Data Structure!!!");
         return true;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
     }
 }
--- src/test/java/com/bridgelabz/datastructure/MyNodeTest.java
+++ src/test/java/com/bridgelabz/datastructure/MyNodeTest.java
@@ -2,12 +2,24 @@ package com.bridgelabz.datastructure;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class MyNodeTest {
     @BeforeClass
     public static void beforeClass(){
-        MyNode myNode = new MyNode();
+        MyNode myNode = new MyNode(56);
         boolean result = myNode.printWelcomeMessage();
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenThreeNumbersWhenLinkedShouldPassLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
         Assert.assertTrue(result);
     }
 }
