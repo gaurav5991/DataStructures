388b180d9d22e307001312af8ee2a9f3b4c4aeb3
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-12 23:55:49 +0530

    [Gaurav]ADDED: Search Element in Binary Tree

--- src/main/java/com/bridgelabz/datastructure/MyBinaryTree.java
+++ src/main/java/com/bridgelabz/datastructure/MyBinaryTree.java
@@ -3,11 +3,11 @@ package com.bridgelabz.datastructure;
 public class MyBinaryTree<K extends Comparable<K>> {
 
     private MyBinaryNode<K> root;
-
+    /*Add Node in BST*/
     public void add(K key) {
         this.root = this.addRecursively(root, key);
     }
-
+    /*Add Recursively*/
     private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
         if (current == null)
             return new MyBinaryNode<>(key);
@@ -21,12 +21,28 @@ public class MyBinaryTree<K extends Comparable<K>> {
         }
         return current;
     }
-
+    /*Get Size*/
     public int getSize() {
         return this.getSizeRecursive(root);
     }
-
+    /*Get Size Recursively*/
     private int getSizeRecursive(MyBinaryNode<K> current) {
         return (current) == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+    }
+    /* Search Node in Binary Tree*/
+    public boolean search(K key) {
+        return (searchRecursive(root, key) != null);
+    }
+
+    private MyBinaryNode<K> searchRecursive(MyBinaryNode<K> current, K key) {
+        if (current == null)
+            return null;
+        else if (current.key.compareTo(key) < 0)
+            return searchRecursive(current.right, key);
+        else if (current.key.compareTo(key) < 0)
+            return searchRecursive(current.left, key);
+        else
+            return current;
+
     }
 }
